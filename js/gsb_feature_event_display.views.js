c22437bf16b166017254d449232a41f78c5bef2f
Tim Plunkett <git@plnktt.com>
2013-06-07 14:26:19 -0700

    Add extra classes to admission events for theming.

--- js/gsb_feature_event_display.views.js
+++ js/gsb_feature_event_display.views.js
@@ -6,9 +6,18 @@
  */
 Drupal.behaviors.gsb_feature_event_display_admission_events = {
   attach: function (context, settings) {
-    var $view = $('.view-admission-events');
-    if ($view.length == 0) {
-      return;
+    var $view;
+    var $context = $(context);
+    // The view is either the context itself.
+    if ($context.hasClass('view-admission-events')) {
+      $view = $context;
+    }
+    else {
+      // Or the view is in the context.
+      $view = $context.find('.view-admission-events');
+      if ($view.length == 0) {
+        return;
+      }
     }
     // Hide the date and sort widgets.
     $view.find('.views-widget-filter-field_event_date_value, .views-widget-sort-order').hide();
@@ -17,6 +26,7 @@ Drupal.behaviors.gsb_feature_event_display_admission_events = {
     var $upcomingButton = $('<div class="event-button event-button-upcoming">' + Drupal.t('Admission Events') + '</div>');
     var $pastButton = $('<div class="event-button event-button-past">' + Drupal.t('Past Admission Events') + '</div>');
     $view.find('.event-buttons').once('event-buttons').append($upcomingButton, $pastButton);
+    $('.event-button-' + settings.gsb_feature_event_display.admission_event_active).addClass('active');
 
     // On click, change the date operator and sort order, and trigger submit.
     $upcomingButton.click(function () {
